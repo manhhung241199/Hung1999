b7fc0b565a921073ad89db385b856aceee027291
hungvu241199 <hungvu5877@gmail.com>
2019-11-26 12:05:39 +0700

    h

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="jumbotron text-center">
+	<h1 style="color: red;size: 100">website from FBIt</h1>
+</div>
+<a href="view.php" class="col-sm-4" style="">View profile</a>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title></title>
+</head>
+<body>
+<?php 
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$sql = "select * from products";
+$stmt = $pdo->prepare($sql);
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+
+
+ ?>
+
+ <ul>
+    <?php 
+    foreach ($resultSet as $row) {
+        echo "<li>".
+        $row["name"]. '--'.$row["price"]
+        ."</li>";
+    }
+
+     ?>
+ </ul>
+
+
+</body>
+</html>
